f61321d9ee3ff27929dd11341b66a8ee3f712ea8
Susmita-Buragohain <susmitaburagohain@gmail.com>
2024-10-23 00:19:08 +0530

    Create Task2.c

---
+++ Task[Date 22.10.2024]/Task2.c
@@ -0,0 +1,38 @@
+Task 2: Array of Structures
+
+	•	Objective: Extend the previous task by creating an array of Student structures. Write a program that allows the user to input information for multiple students and then displays the details for each student.
+	•	Requirements:
+	•	Define an array of Student structures.
+	•	Use a loop to gather input for multiple students.
+	•	Create a function to display all student details.
+
+CODE
+
+#include <stdio.h>
+
+struct student{
+    char name[50];
+    int age;
+    char grade [50];
+};
+
+int main() {
+    struct student x[5];
+    for (int i = 0; i < 5; i++) {
+        printf("Enter details for students%d:\n", i + 1);
+        printf("Enter Name: ");
+        scanf("%s", x[i].name);
+        printf("Enter Age: ");
+        scanf("%d", &x[i].age);
+        printf("Enter Grade: ");
+        scanf("%s", x[i].grade);
+    }
+    display_student(x, 5);
+}
+ display_student(struct student x[], int n) {
+    printf("Student Details:\n");
+    for (int i = 0; i < n; i++) {
+        printf("Student%d: Name: %s, Age: %d, Grade: %s\n", 
+               i + 1, x[i].name,x[i].age, x[i].grade);
+    }
+}
