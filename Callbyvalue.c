7854a1b3bd6efa9d9b656cca2c77b1ff1a239db4
Susmita-Buragohain <susmitaburagohain@gmail.com>
2024-10-22 23:21:51 +0530

    Create Callbyvalue.c

---
+++ Callbyvalue.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+int x=10,y=20;
+
+printf("Before swap: %d,%d\n",x,y);
+swap(x,y);
+printf("After swap :%d,%d",x,y); 
+}
+swap(int a,int b)
+{
+int temp=a;
+a=b;
+b=temp;
+}
