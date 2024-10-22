55a90f257ebe162bc4328832b974557527acc083
Susmita-Buragohain <susmitaburagohain@gmail.com>
2024-10-23 00:25:27 +0530

    Create callbyvalue.c

---
+++ Task[Date 22.10.2024]/callbyvalue.c
@@ -0,0 +1,17 @@
+Swap two numbers using call by value
+
+#include<stdio.h>
+main()
+{
+int x=4,y=7;
+
+printf("Before swap: %d,%d\n",x,y);
+swap(x,y);
+printf("After swap :%d,%d",x,y); 
+}
+swap(int a,int b)
+{
+int temp=a;
+a=b;
+b=temp;
+}
