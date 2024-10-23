e59ce582eff7da34e998365bc2add56afcdcebf2
Susmita-Buragohain <susmitaburagohain@gmail.com>
2024-10-23 22:48:30 +0530

    Update Task2.c

--- Task2.c
+++ Task2.c
@@ -3,12 +3,13 @@
 struct student{
     char name[50];
     int age;
-    char grade [50];
+    char grade[50];
 };
-
+void display_student(struct student x[],int n);
 int main() {
     struct student x[5];
-    for (int i = 0; i < 5; i++) {
+    int i;
+    for ( i = 0; i < 5; i++) {
         printf("Enter details for students%d:\n", i + 1);
         printf("Enter Name: ");
         scanf("%s", x[i].name);
@@ -18,10 +19,12 @@ int main() {
         scanf("%s", x[i].grade);
     }
     display_student(x, 5);
+    return 0;
 }
- display_student(struct student x[], int n) {
+ display_student(struct student x[], int n){
+ 	int i;
     printf("Student Details:\n");
-    for (int i = 0; i < n; i++) {
+    for ( i = 0; i < n; i++) {
         printf("Student%d: Name: %s, Age: %d, Grade: %s\n", 
                i + 1, x[i].name,x[i].age, x[i].grade);
     }
